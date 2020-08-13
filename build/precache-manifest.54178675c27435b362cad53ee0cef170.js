57de6af06bdbf684106005862171e0a5f6dd54ab
surajgurung <microbiosuraj@gmail.com>
2020-08-13 12:14:53 -0400

    some change on contact.js

---
+++ build/precache-manifest.54178675c27435b362cad53ee0cef170.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29e9c3f6be10c512d66012083746b83",
+    "url": "/responsivePortfolio/index.html"
+  },
+  {
+    "revision": "75d2812c46347d7e5632",
+    "url": "/responsivePortfolio/static/css/main.4660ce5f.chunk.css"
+  },
+  {
+    "revision": "3ae126f0c959ef8e3acf",
+    "url": "/responsivePortfolio/static/js/2.d34c26f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/responsivePortfolio/static/js/2.d34c26f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d2812c46347d7e5632",
+    "url": "/responsivePortfolio/static/js/main.9e49ff91.chunk.js"
+  },
+  {
+    "revision": "262110009f58e92c783c",
+    "url": "/responsivePortfolio/static/js/runtime-main.63631d60.js"
+  },
+  {
+    "revision": "ef2c76f26bbdeee4a6f8bd65114addbf",
+    "url": "/responsivePortfolio/static/media/bkgd1.ef2c76f2.png"
+  },
+  {
+    "revision": "dc96718636810c179216f4ce11adae45",
+    "url": "/responsivePortfolio/static/media/github1.dc967186.png"
+  },
+  {
+    "revision": "097b77494452f9352630bcf79697db4d",
+    "url": "/responsivePortfolio/static/media/insta.097b7749.png"
+  },
+  {
+    "revision": "65dac773723eb08d56913d8588384cfd",
+    "url": "/responsivePortfolio/static/media/mail.65dac773.png"
+  },
+  {
+    "revision": "254907e7bfc3cba2d63b403cdd4fc876",
+    "url": "/responsivePortfolio/static/media/suraj.254907e7.JPEG"
+  },
+  {
+    "revision": "d2d3149a44e0d10ef17dcb6485451d6a",
+    "url": "/responsivePortfolio/static/media/surajresume.d2d3149a.pdf"
+  }
+]);
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,30 +9,19 @@ import insta from './Images/insta.png';
 export const Contact = () => {
     
     return (
-  
         <div>
-  
-          <h2  style={{marginTop: '30px',marginLeft:'30px' }}>Contact Me</h2>
-              <div className="row">
-                <div className="col-sm-4" style={{margin: '30px'}}>
-                     <a href="mailto: microbiosuraj@gmail.com" class="card-link"><img src={mail} alt="mail"
-                    width="30px" height="30px" hspan="20"/></a>
-                </div>
-                <div className="col-sm-4" style={{margin: '30px'}}>
-                <a href="https://www.linkedin.com/in/suraj-gurung-38069419a/"><img src={linkendin}
-                    alt="linkedin image" width="30px" height="30px" hspan="20"/></a>
-                    </div>
-                <div className="col-sm-4 " style={{margin: '30px'}}>
-                <a href="https://github.com/surajbgurung"><img src={github} alt="github image"
-                    width="30px" height="30px" hspan="20"/></a>
-                    </div>
-                <div className="col-sm-4"style={{margin: '30px'}}>
-                <a href="https://www.instagram.com/jarus_grg/?hl=en"><img src={insta} alt="insta image"
-                    width="30px" height="30px" hspan="20"/></a>
-                    </div>
-              </div>
-            
+        <h2  style={{marginTop: '30px',marginLeft:'30px' }}>Contact Me</h2>
+            <div className="row">
+                   <a href="mailto: microbiosuraj@gmail.com" class="card-link" style={{margin: '20px' }}><img src={mail} alt="mail"
+                  width="40px" height="40px" hspan="20"/></a>
+              <a href="https://www.linkedin.com/in/suraj-gurung-38069419a/" style={{margin: '20px' }}><img src={linkendin}
+                  alt="linkedin image" width="35px" height="35px" hspan="20"/></a>
+              <a href="https://github.com/surajbgurung" style={{margin: '20px' }}><img src={github} alt="github image"
+                  width="40px" height="40px" hspan="20"/></a> 
+              <a href="https://www.instagram.com/jarus_grg/?hl=en" style={{margin: '20px' }}><img src={insta} alt="insta image"
+                  width="40px" height="40px" hspan="20"/></a>
             </div>
+          </div>
          )
     
 }
